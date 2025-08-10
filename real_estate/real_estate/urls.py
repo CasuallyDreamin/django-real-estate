a78ae3e8bfcc7b43344470d31372d8be4bcb43f9
CasuallyDreamin <ysn.arambash@gmail.com>
2025-08-10 11:04:45 -0400

    Implemented Main image upload functionality for houses

--- real_estate/real_estate/urls.py
+++ real_estate/real_estate/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
@@ -12,3 +15,6 @@ urlpatterns = [
     path('', RedirectView.as_view(url="/dashboard/"))
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

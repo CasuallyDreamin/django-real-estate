8614573a4c0d2cca12a8818bdda13e101f181b69
CasuallyDreamin <ysn.arambash@gmail.com>
2025-08-11 11:08:47 -0400

    Implemented the dashboard FIxed css pathing issues

--- real_estate/dashboard/views.py
+++ real_estate/dashboard/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from houses.models import House
 
-def dashboard(request):
-    return render(request, 'dashboard/home.html')
+class DashboardView(TemplateView):
+    template_name = 'dashboard/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['total_houses'] = House.objects.count()
+        context['available'] = House.objects.filter(status='available').count()
+        context['sold'] = House.objects.filter(status='sold').count()
+        context['pending'] = House.objects.filter(status='pending').count()
+        context['recent_houses'] = House.objects.order_by('-created_at')[:5]
+
+        return context

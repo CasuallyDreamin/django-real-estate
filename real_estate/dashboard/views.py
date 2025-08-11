ce2cd5e96e84ac0bc161c745dff3b203a1dba96f
CasuallyDreamin <ysn.arambash@gmail.com>
2025-08-11 12:00:39 -0400

    Dashboard bugfix

--- real_estate/dashboard/views.py
+++ real_estate/dashboard/views.py
@@ -1,16 +1,19 @@
 from django.views.generic import TemplateView
 from houses.models import House
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class DashboardView(TemplateView):
+class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        context['total_houses'] = House.objects.count()
-        context['available'] = House.objects.filter(status='available').count()
-        context['sold'] = House.objects.filter(status='sold').count()
-        context['pending'] = House.objects.filter(status='pending').count()
-        context['recent_houses'] = House.objects.order_by('-created_at')[:5]
+        user = self.request.user
+        user_houses = House.objects.filter(agent=user)
+        
+        context['total_houses'] = user_houses.count()
+        context['available'] = user_houses.filter(status='available').count()
+        context['sold'] = user_houses.filter(status='sold').count()
+        context['pending'] = user_houses.filter(status='pending').count()
+        context['recent_houses'] = user_houses.order_by('-created_at')[:5]
 
         return context

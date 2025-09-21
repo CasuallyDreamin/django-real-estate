cc53c9532ba995e124a0e2c600f48a5dab47e6d3
CasuallyDreamin <ysn.arambash@gmail.com>
2025-09-21 11:10:05 -0400

    chore: added whitenoise middleware to serve the css

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from os import path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -124,12 +125,12 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATIC_ROOT = path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Templates
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
 
-# Media files (if using uploads)
+# Media files (if using uploads)a
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
